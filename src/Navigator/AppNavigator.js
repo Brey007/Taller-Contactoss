2569add4566fa3dc7cbde070876565ca826c9875
Brey007 <sirope44@gmail.com>
2025-08-02 15:15:48 -0500

    Codeamos AppNavigator.js t App.js para ver nuestra app

---
+++ src/Navigator/AppNavigator.js
@@ -0,0 +1,28 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import ContactListScreen from '../screens/ContactListScreen';
+import AddContactScreen from '../screens/AddContactScreen';
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="ContactList"
+          component={ContactListScreen}
+          options={{ title: 'Contactos' }}
+        />
+        <Stack.Screen
+          name="AddContact"
+          component={AddContactScreen}
+          options={{ title: 'Añadir Contacto Nuevo' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
